ba50bb909d1b4507dc74e30311bf792f4e14278b
ErgangDong <821220992@qq.com>
2016-10-22 11:10:35 +0800

    Update main.js

--- demo/js/main.js
+++ demo/js/main.js
@@ -1,10 +1,13 @@
+//配置项
 requirejs.config({
+	//配置路径和别名
 	baseUrl:'js/libs',
 	paths:{
 		'jquery':'jquery/jquery-3.1.0.min',
 		'wocao':'wocao',
 	}
 })
+//加载模块
 requirejs(['jquery','wocao'],function($,wocao){
 	$('body').css('background','blue');
 	console.log(wocao.isEqual(1,1));//true
